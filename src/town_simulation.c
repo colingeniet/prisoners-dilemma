666a1d7c447b2ca3e02aaeba5909efbcfa18093c
Colin Geniet <colin.geniet@gmail.com>
2018-01-04 12:04:20 +0100

    town now sends data to monitor.

--- src/town_simulation.c
+++ src/town_simulation.c
@@ -15,6 +15,8 @@ struct population_data {
     sem_t *done;
 };
 
+int step;
+
 void *population_process(void *data) {
     struct population_data *arg = (struct population_data *)data;
     population_parallel(arg->town, arg->pop_lock, arg->next, arg->done);
@@ -22,10 +24,9 @@ void *population_process(void *data) {
 }
 
 // just quit as soon as a message is sent, or connection is closed.
-void *monitor_com(void *data) {
-    int socket = open_listen_socket(4000);
-    int fd = wait_for_client(socket);
-
+// it's stupid, but it works
+void *monitor_com(void *_fd) {
+    int fd = (int)_fd;
     char c;
     read(fd, &c, 1);
     exit(0);
@@ -45,17 +46,22 @@ int main(int argc, char **argv) {
     data.next = &next;
     data.done = &done;
 
+    int socket = open_listen_socket(4000);
+    int fd = wait_for_client(socket);
+
     pthread_t pop_t, mon_t;
     pthread_create(&pop_t, NULL, population_process, (void*)&data);
-    pthread_create(&mon_t, NULL, monitor_com, NULL);
+    pthread_create(&mon_t, NULL, monitor_com, (void*)fd);
 
-    for(int step=0;;step++) {
-        printf("step %d\n", step);
+    FILE *mon = fdopen(fd, "w");
+
+    for(step=0;;step++) {
+        fprintf(mon, "step %d\n", step);
         for(int i=0; i<town->n_strategies; i++) {
             if(!town->allowed[i]) continue;
-            printf("%s\t%ld\n", town->strategies[i].short_name, town->population[i]);
+            fprintf(mon, "%s\t%ld\n", town->strategies[i].short_name, town->population[i]);
         }
-        printf("\n");
+        fprintf(mon, "\n");
         sem_post(&next);
         sleep(1);
         sem_wait(&done);
